27447fa6d35ec248874debe575041161b6b8ba23
MickaelNabet <71086733+MickaelNabet@users.noreply.github.com>
2021-10-07 09:53:53 +0200

    Add files via upload

---
+++ main.java
@@ -0,0 +1,15 @@
+package fr.dauphine.javaavance.td1;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("la france");
+	}
+
+	int foo;	
+	public void setFoo(int foo) {
+		this.foo = foo;
+	}
+
+}
