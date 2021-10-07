aec1d33a168544d00b1035326637f67bbabdca35
MickaelNabet <71086733+MickaelNabet@users.noreply.github.com>
2021-10-07 09:53:06 +0200

    Add files via upload

---
+++ TestPoint.java
@@ -0,0 +1,9 @@
+package fr.dauphine.javaavance.td1;
+
+public class TestPoint {
+	
+	public static void main(String[] args) {
+		Point p = new Point(1,2);
+		System.out.println(p.getX()+" "+p.getY());
+	}
+}
