467e8ecc8de635aa3fec0581f72382e980c644cf
MickaelNabet <71086733+MickaelNabet@users.noreply.github.com>
2021-10-07 22:52:21 +0200

    Delete TestPoint.java

--- TestPoint.java
+++
@@ -1,9 +0,0 @@
-package fr.dauphine.javaavance.td1;
-
-public class TestPoint {
-	
-	public static void main(String[] args) {
-		Point p = new Point(1,2);
-		System.out.println(p.getX()+" "+p.getY());
-	}
-}
